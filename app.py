5f0565a9e13fd5b568b3abbe0ef114e40913b75d
tanvir039 <tanvir039@gmail.com>
2026-06-04 13:26:16 +0100

    changed env variables and Welcome message

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ from redis import Redis
 import os
 
 app = Flask(__name__)
-redis = Redis(host=os.environ.get('REDIS_HOST', 'redis'), 
-              port=int(os.environ.get("REDIS_PORT", 6379)))
+
+redis_host = os.getenv('REDIS_HOST', 'redis')
+redis_port = int(os.getenv('REDIS_PORT', 6379))
+redis = Redis(host = redis_host, port = redis_port)
 
 @app.route('/')
-def hello_world():
-    return f'Hello, World!'
+def welcome():
+    return f'Welcome to the Docker Challenge! This is a simple Flask application that uses Redis to count the number of visits to this page. The Redis server is running on {redis_host}:{redis_port}.'
 
 @app.route('/count')
 def index():
